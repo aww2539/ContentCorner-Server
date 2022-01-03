a8425937dc18369abe02baabbdcfff3e408b4651
Alex Woodall <aww2539@gmail.com>
2022-01-03 10:57:07 -0600

    whitelisted front end url

--- CreatorCorner/settings.py
+++ CreatorCorner/settings.py
@@ -87,7 +87,8 @@ REST_FRAMEWORK = {
 }
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://creator-corner-client.herokuapp.com/'
 )
 
 WSGI_APPLICATION = 'CreatorCorner.wsgi.application'
